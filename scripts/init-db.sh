524e3ae4042676e2c35e06855a89f79a413f56df
yanioconjota <janioisacura@gmail.com>
2024-09-05 13:52:37 -0300

    force overwrite

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -7,7 +7,7 @@
 apt-get update && apt-get install -y curl apt-transport-https gnupg2 && \
     curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
     curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
-    apt-get update && ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev && \
+    apt-get update && ACCEPT_EULA=Y apt-get install -y --force-overwrite mssql-tools unixodbc-dev && \
     echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc && \
     source ~/.bashrc
 
