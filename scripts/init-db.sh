180e21ba1b0c8aa4932cde599e032a323efe6781
yanioconjota <janioisacura@gmail.com>
2024-11-26 05:40:46 -0300

    working on new docker config

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -7,7 +7,7 @@
 sleep 30
 
 # Ejecutar script SQL para restaurar la base de datos
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Elasticp!" -d master -i /scripts/restore-database.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P "$SA_PASSWORD" -Q "RESTORE DATABASE AdventureWorks2017 FROM DISK = '/scripts/backups/AdventureWorks2017.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2017.mdf', MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks2017_Log.ldf';"
 
 # Mantener el proceso de SQL Server en primer plano
 wait
